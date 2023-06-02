d0f58a6ed379730fda3a11490ed6388400486ae9
krishna <krishna5.xworkz@gmail.com>
2023-06-02 12:40:17 +0530

    test uploded

---
+++ Test02.java
@@ -0,0 +1,100 @@
+Test
+1) Explain java and javac 
+java command helps to run the program
+
+javac 
+
+java "c" means java compilation where compiler converts the source code to bite code 
+
+2) explain JDK,JRE,JVM
+JDK:- java development kit
+    JDK is the combination of JRE and JVM 
+	it contains set of libraies
+	
+JRE:- java run time environment
+   *it is used to excute the code
+   * it is combination of JDK and development tools
+   
+JVM:- java virtual machine 
+	* 
+
+3)List the limitation of an array
+	array is list of data
+	we can store n number of data or values in a array list
+	we should store only same type of data in an array list
+	example:- if we are storing a String type of data we use String array where we should enter only name
+				if we try to store int type of data type in String array we get error
+
+
+4)Explain class,object and signature of the class
+	it has some state and bheaviours
+Class is key word in java,it a blue print of an object.
+
+Object is the instance of a class 
+it is used to create copies of class by using the keyword call "new".
+when an object is created it occupies some storage
+
+5)Differnce between the .java and .class flie
+.java is a source file where as .class is an byte code
+
+6)Explain the method signature 
+method is a block of code which performs some specific funtionality of an class
+
+	method signature :-public void main(){
+										}
+ here public is called as acces specifier ,void is the return type ,main is the method name,()paranthises followed by "{}"
+
+7) What is java
+*java is a object oriented program 
+* java is mainly used to build application like mobile application,E-commerce ,banking application etc
+* java can run in any platform so it is called as platform independent 
+* java is easy to understand 
+
+
+8) List the java IDE's
+  java IDE's means java application to write the program
+  * Eclipse
+  *intellij IDEA
+  *Notepad++ 
+  
+9)What is variables explain its type
+variables are the containers which is used to store the data 
+its types are 
+*Static variables - declared  inside the class outside the main method 
+*local variables - declared inside the class and within the main method
+*instance variables - declared outside the main method without using Static keyword
+*parameterised variables
+
+10)List 15 keywords
+*byte,int,float,double,char,short,long,boolean,super,this,if,new,Extend,import,public,private,procted
+
+11) What is a class variables!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+12) What is data type and its type
+datatype is the type of data stored in a variables
+there are two types Primitive and Non primitive 
+Primitive are 8 and Non Primitive means n number that is user defined they are class names ,String Array etc
+
+13) What is dot operator
+it is used to access the members and properties from other class 
+
+14)List the features of java
+object oriented
+platform independent runs in all platforms
+multi threaded
+
+15)What is the base class of all the classes 111!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+16) What is Method overloading with an example
+ Method overloading means a two methods having same Method name but different in parameters
+ 
+ example:- public void test(int){
+	 System.out.Println("calling test method")
+ }
+
+		public void test(String){
+	 System.out.Println("calling test method")
+ }
+
+
+
